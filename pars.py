b16aac84d9ff64a4942f080f4609854ac3661f69
Shishel <86182195+Shishel@users.noreply.github.com>
2025-04-03 00:08:36 +0300

    Update pars.py

--- pars.py
+++ pars.py
@@ -75,4 +75,4 @@ if __name__ == "__main__":
     # Поток для парсинга данных
     threading.Thread(target=fetch_data, daemon=True).start()
     print("Сервер запускается...")
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=True, use_reloader=False, port=8000)
