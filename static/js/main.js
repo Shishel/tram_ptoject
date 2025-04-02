46b5700a817f1315cd71f7b91209cffccd095039
Shishel <86182195+Shishel@users.noreply.github.com>
2025-04-02 20:17:10 +0300

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -20,7 +20,7 @@ function updateRouteType(type) {
 
 // Функция для обновления меток транспорта
 function updateTrams() {
-    fetch('https://6f4d-87-121-58-199.ngrok-free.app/api/tram')
+    fetch('https://wicked-geckos-flash.loca.lt')
         .then(response => response.json())
         .then(data => {
             placemarks.forEach(placemark => map.geoObjects.remove(placemark));
